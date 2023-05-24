06f8fba51e02045e580bde41a44d4070217d79db
lgxxhi <luisgsilva95@gmail.com>
2023-05-24 12:56:56 -0400

    created searchbar static

--- src/App.js
+++ src/App.js
@@ -28,10 +28,32 @@ function App() {
       </div>
     );
   }
+  function SearchBar() {
+    return (
+      <div className="search-container">
+        <div className="input-group">
+          <input
+            type="search"
+            className="form-control rounded"
+            placeholder="Search..."
+            aria-label="Search"
+            aria-describedby="search-addon"
+          />
+          <button type="button" className="btn btn-outline-primary">
+            search
+          </button>
+        </div>
+        <div className="alert alert-light" role="alert">
+          No Search Results Yet!, Please submit a search above!
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="App">
       <Router>
         <CreateNavBar />
+        <SearchBar />
         <Routes>
           <Route path="/home" />
           <Route path="/about" />
