de72c7bd6bb26626430f4aff597d0e08a965170d
Jameel Ibrahim <jameelibrahim@Jameels-MacBook-Air.local>
2023-05-24 12:56:53 -0400

    added env

--- src/App.js
+++ src/App.js
@@ -1,7 +1,68 @@
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  function CreateNavBar() {
+    return (
+      <div className="navBar">
+        <ul>
+          <li>Youtube</li>
+          <li>
+            <NavLink
+              className={({ isActive }) => (isActive ? "active-link" : "")}
+              to="/home"
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={({ isActive }) => (isActive ? "active-link" : "")}
+              to="/about"
+            >
+              About
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+  function SearchBar() {
+    return (
+      <div className="search-container">
+        <div className="input-group">
+          <input
+            type="search"
+            className="form-control rounded"
+            placeholder="Search..."
+            aria-label="Search"
+            aria-describedby="search-addon"
+          />
+          <button type="button" className="btn btn-outline-primary">
+            search
+          </button>
+        </div>
+        <div className="alert alert-light" role="alert">
+          No Search Results Yet!, Please submit a search above!
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="App">
+      <Router>
+        <CreateNavBar />
+        <SearchBar />
+        <Routes>
+          <Route path="/home" />
+          <Route path="/about" />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
