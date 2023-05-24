3a0a59c909e8320a7dcdf8045001064221051b59
lgxxhi <luisgsilva95@gmail.com>
2023-05-24 11:16:24 -0400

    Created navBar with semi working links

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  function CreateNavBar() {
+    return (
+      <div className="navBar">
+        <ul>
+          <li>Youtube</li>
+          <li>
+            <NavLink
+              className={({ isActive }) => (isActive ? "active-link" : "")}
+              to="/home"
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={({ isActive }) => (isActive ? "active-link" : "")}
+              to="/about"
+            >
+              About
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+  return (
+    <div className="App">
+      <Router>
+        <CreateNavBar />
+        <Routes>
+          <Route path="/home" />
+          <Route path="/about" />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
