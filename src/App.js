0739e80a101b3562e1186e0ce1fc814e5a9acb3f
Jameel Ibrahim <jameelibrahim@Jameels-MacBook-Air.local>
2023-05-25 12:38:28 -0400

    craeted card component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-
+import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { NavLink, Link } from "react-router-dom";
-import "./App.css";
+import axios from "axios";
+import React, { useState } from "react";
 
 function App() {
   function CreateNavBar() {
@@ -30,27 +31,65 @@ function App() {
     );
   }
   function SearchBar() {
+    const [results, setResults] = useState([]);
+    const [search, setSearch] = useState("");
+
+    let API = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${search}&key=AIzaSyCdlC9flaUqKX9b2g9lbIzb4Us0PcEU7nQ&maxResults=25`;
+
+    async function handleFetch() {
+      try {
+        let result = await axios.get(API);
+
+        setResults(result.data.items);
+        
+      } catch (e) {
+        console.log(e);
+      }
+    }
+
     return (
       <div className="search-container">
         <div className="input-group">
-          <input
-            type="search"
-            className="form-control rounded"
-            placeholder="Search..."
-            aria-label="Search"
-            aria-describedby="search-addon"
-          />
-          <button type="button" className="btn btn-outline-primary">
-            search
-          </button>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleFetch();
+            }}
+          >
+            <input
+              onChange={(e) => setSearch(e.target.value)}
+              type="search"
+              className="form-control rounded"
+              placeholder="Search..."
+              aria-label="Search"
+              aria-describedby="search-addon"
+            />
+            <button type="button" className="btn btn-outline-primary">
+              search
+            </button>
+          </form>
         </div>
         <div className="alert alert-light" role="alert">
           No Search Results Yet!, Please submit a search above!
         </div>
+
+        {results.map(({ etag, snippet }) => {
+          return (
+            <div key={etag} className="card" style={{ width: "18rem" }}>
+              <img
+                src={snippet.thumbnails.high.url}
+                className="card-img-top"
+                alt={snippet.channelTitle}
+              />
+              <div className="card-body">
+                <h2 className="card-text">{snippet.title}</h2>
+              </div>
+            </div>
+          );
+        })}
       </div>
     );
   }
-
   return (
     <div className="App">
       <Router>
