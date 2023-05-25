36de52245224feab819328fa2214ce8b71070567
lgxxhi <luisgsilva95@gmail.com>
2023-05-25 13:01:06 -0400

    Added some css to header

--- src/App.js
+++ src/App.js
@@ -6,25 +6,42 @@ function App() {
   function CreateNavBar() {
     return (
       <div className="navBar">
-        <ul>
-          <li>Youtube</li>
-          <li>
-            <NavLink
-              className={({ isActive }) => (isActive ? "active-link" : "")}
-              to="/home"
+        <nav className="navbar navbar-expand-lg ">
+          <div className="container-fluid">
+            <a className="navbar-brand text-white" href="#">
+              YouTube
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
             >
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              className={({ isActive }) => (isActive ? "active-link" : "")}
-              to="/about"
-            >
-              About
-            </NavLink>
-          </li>
-        </ul>
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item ">
+                  <NavLink
+                    className="nav-link active text-white"
+                    aria-current="page"
+                    to={"/"}
+                  >
+                    Home
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link text-white" to="/about">
+                    About
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
       </div>
     );
   }
